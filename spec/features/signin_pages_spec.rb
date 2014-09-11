5da9b0351edae2bb1ede2f2f2cb8575e6036f9f4
Patrick David & Marina Neumann <student@epicodus.com>
2014-09-11 15:09:43 -0700

    pass capybara test that allows a user to register for photosharing

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe "the login process" do
+
   it "logs a user in who uses the right password" do
     visit '/login'
     user = User.create(:email => 'user@example.com', :password => 'password')
@@ -17,5 +18,14 @@ describe "the login process" do
     fill_in 'Password', :with => 'notpassword'
     click_button 'Log In'
     expect(page).to have_content 'invalid'
+  end
+
+  it "will successfully allow a user to sign up" do
+    visit '/signup'
+    fill_in 'Email', with: 'user@email.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign Up'
+    expect(page).to have_content 'Photo'
   end
 end
