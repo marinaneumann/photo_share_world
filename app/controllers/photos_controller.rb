2f6d288c9c0fcc4180a5c0bac0d33960ba8f67f6
Patrick David & Marina Neumann <student@epicodus.com>
2014-09-11 11:18:26 -0700

    make a root to the photos index page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,28 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(params[:photo])
+    if @photo.save
+      redirect_to photo_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ get 'signup', to: 'users#new', as: 'signup'
 get 'login', to: 'sessions#new', as: 'login'
 get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  root to: 'photos#index'
   resources :users
   resources :sessions
-
+  resources :photos
 
 end
